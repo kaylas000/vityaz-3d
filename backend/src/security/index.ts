e2e48cbb99011179188aea380a2700e50d049f54
kaylas000 <kaylas000@yandex.com>
2025-12-15 12:43:21 +0300

    Security: Core security system with all protections

---
+++ backend/src/security/index.ts
@@ -0,0 +1,15 @@
+/**
+ * VITYAZ Security System - Core Module
+ * Comprehensive security implementation for production
+ */
+
+export * from './helmet';
+export * from './rateLimit';
+export * from './cors';
+export * from './validation';
+export * from './csrf';
+export * from './jwt';
+export * from './password';
+export * from './sanitize';
+export * from './logging';
+export * from './errorHandler';
